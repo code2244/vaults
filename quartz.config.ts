6682c69dc2fff76d35aa0edbf55324a22bde0789
rick <rickhdev@gmail.com>
2024-04-28 17:56:02 -0400

    Quartz sync: Apr 28, 2024, 5:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "I just wan to Hack...hardware.",
+    pageTitle: "I just want to hack...hardware.",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
